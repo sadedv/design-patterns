40a8a796c7c8269d7746ccd31119475d009a19fa
Tomas Jakubco <tomas.jakubco@gmail.com>
2013-05-26 20:52:11 +0200

    Update

--- src/networking/multicast/ChatWindow.java
+++ src/networking/multicast/ChatWindow.java
@@ -1,15 +1,24 @@
 package networking.multicast;
 
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
-import javax.swing.GroupLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class ChatWindow extends JFrame {
 
     private static final long serialVersionUID = 1L;
+
+    // Components
+    Container pane;
+    JPanel mainPanel;
+    JPanel leftPanel;
+    JPanel rightPanel;
 
     public ChatWindow() {
 
@@ -21,22 +30,45 @@ public class ChatWindow extends JFrame {
         setTitle("Simple Chat Application");
 
         // Initialize components
-        init();
+        initComponents();
+
+        // Layout components
+        layoutComponents();
 
     }
 
-    private void init() {
+    private void initComponents() {
 
-        // Set group layout
-        getContentPane().setLayout(new GroupLayout(getContentPane()));
+        pane = getContentPane();
+        pane.setLayout(new FlowLayout());
 
-        // Add main panel
-        JPanel panel = new JPanel();
-        panel.setSize(getContentPane().getWidth() - 10, getContentPane()
-                .getHeight() - 10);
-        panel.setLocation(5, 5);
-        panel.setBackground(new Color(200, 200, 200));
-        add(panel);
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new GridBagLayout());
+        mainPanel.setBackground(new Color(180, 180, 180));
+        pane.add(mainPanel);
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+
+        leftPanel = new JPanel();
+        leftPanel.setBackground(new Color(200, 200, 200));
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 6;
+        mainPanel.add(leftPanel, c);
+
+        rightPanel = new JPanel();
+        rightPanel.setBackground(new Color(190, 190, 190));
+        c.gridx = 1;
+        c.gridy = 0;
+        c.weightx = 2;
+        mainPanel.add(rightPanel, c);
+    }
+
+    private void layoutComponents() {
+
+        mainPanel.setPreferredSize(new Dimension(pane.getWidth() - 10, pane
+                .getHeight() - 10));
 
     }
 
